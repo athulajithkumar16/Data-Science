fc8f5788d0d7ffde88b77d053a85b5a262f82cbc
Athul K A <athulajithkumar16@gmail.com>
2024-08-31 17:10:31 +0530

    Update q8.py

--- for_loop_practice/q8.py
+++ for_loop_practice/q8.py
@@ -2,3 +2,12 @@
 
 string = input('Enter string : ')
 
+reverse_string = ''
+
+for i in string:
+    reverse_string = i + reverse_string
+
+if string == reverse_string:
+    print('PALINDROME')
+else:
+    print('NOT PALINDROME')
