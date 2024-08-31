1ad37171d4e4c4aee15b8687f69f61ca435181be
Athul K A <athulajithkumar16@gmail.com>
2024-08-31 16:58:21 +0530

    .

---
+++ for_loop_practice/q9.py
@@ -0,0 +1,9 @@
+# Example 9: Python program that accepts a word from the user and reverses it.
+
+word = input('Enter a word : ')
+reversed_ = []
+
+for i in word:
+    reversed_.append(i)
+    
+print(reversed_.s)
