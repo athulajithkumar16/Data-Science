288beac44f350d73193e478df261ae990b03b008
Athul K A <athulajithkumar16@gmail.com>
2024-09-01 16:26:26 +0530

    .

---
+++ for_loop_practice/q10.py
@@ -0,0 +1,11 @@
+# Example 10: Python program to check if a given number is an Armstrong number
+
+# n = 123 
+
+number = int(input('Enter a number : '))
+sum = 0
+# print(len(str(number)))
+
+for i in range(1,len(str(number))+1):
+    sum += i^
+    print(i)
