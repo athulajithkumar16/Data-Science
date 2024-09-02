dfa6f1db9b08baaf930431f55171e53658040844
Athul K A <athulajithkumar16@gmail.com>
2024-09-02 08:55:49 +0530

    .

--- StringOperations/indexing.py
+++ StringOperations/indexing.py
@@ -1,4 +1,5 @@
 name = "Data Science"
+print(f'Length of {name} is {len(name)}')
 
 print(name[0:5])
 print(name[2:5])
@@ -6,6 +7,11 @@ print(name[7:])
 print(name[:7])
 
 new = 'hello'
+print(f'Length of {new} is {len(new)}')
+
 
 print(new.replace('l','R', 2))
-print(new[:3]+'R'+new[-1])
+
+print(new[:3]+'R'+new[4:])
+
+print(len('five'))
---
+++ StringOperations/stringoper.py
@@ -0,0 +1,6 @@
+
+
+
+name = 'Examination'
+
+print(f'{name[0].lower()}{len(name)}{name[-1].upper()}')
