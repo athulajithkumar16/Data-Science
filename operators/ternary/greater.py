7ac467f3f91f568d4ca6adae8fc7ca5c05f72935
Athul K A <athulajithkumar16@gmail.com>
2024-09-03 09:57:51 +0530

    .

---
+++ operators/ternary/greater.py
@@ -0,0 +1,4 @@
+num1 = int(input('Enter first number : '))
+num2 = int(input('Enter second number : '))
+
+print(f'{num1} is greater than {num2}' if num1 > num2 else f'{num2} is greater than {num1}')
--- operators/ternary/positive.py
+++ operators/ternary/positive.py
@@ -2,6 +2,8 @@
 num = int(input('Enter a number : '))
  
 result = "Positive" if num > 0 else "Negative"
+
 # result = "Positive" if num > 0 elif num == 0 "Zero" else "Negative"               WRONG
 # Return positive if num > 0 else negative
+
 print(result)
