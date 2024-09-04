017372ffa83b6ba2a45e2bd9e94b546eb74d0e84
Athul K A <athulajithkumar16@gmail.com>
2024-09-04 09:04:09 +0530

    .

--- operators/ternary/div_by_3.py
+++
@@ -1,3 +0,0 @@
-num = int(input('Enter a number : '))
-
-print("Divisible by both 3 or 5" if num%3==0 and num%5==0 else ("Divisible by either 3 or 5" if num%3==0 or num%5==0 else "Not divisible by 3 or 5"))
