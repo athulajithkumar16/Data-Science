ccd020a0971b53e3c30bbc4729292ed1ebedec52
Athul K A <athulajithkumar16@gmail.com>
2024-09-04 09:49:58 +0530

    .

---
+++ loops/while loop/intro.py
@@ -0,0 +1,8 @@
+i = 0
+while i<5:
+    print('hello')
+    i+=1
+
+while i>=0:
+    print(i)
+    i -= 1
---
+++ loops/while loop/sample.py
@@ -0,0 +1,11 @@
+# while loop : we can execute a set of statements as long as the condition is True
+
+# initialization
+# while condition:
+#     statement
+#     increment / decrement
+
+i = 10
+while i > 0:
+    print(i)
+    i -= 1
