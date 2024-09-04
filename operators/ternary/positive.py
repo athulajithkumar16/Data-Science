a0a5e7d945c441e46f8690093cb40fe3f20d964a
Athul K A <athulajithkumar16@gmail.com>
2024-09-04 08:29:19 +0530

    .

--- operators/ternary/positive.py
+++ operators/ternary/positive.py
@@ -4,6 +4,6 @@ num = int(input('Enter a number : '))
 result = "Positive" if num > 0 else "Negative"
 
 # result = "Positive" if num > 0 elif num == 0 "Zero" else "Negative"               WRONG
-# Return positive if num > 0 else negative
+# Return positive if num > 0 else return negative
 
 print(result)
