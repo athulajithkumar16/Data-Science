c6e87249bb37726dc1f29b73642899490247a61b
Athul K A <athulajithkumar16@gmail.com>
2024-09-05 09:53:14 +0530

    .

--- loops/while loop/sum.py
+++ loops/while loop/sum.py
@@ -1,4 +1,11 @@
 i, sum = 1,0
 
-while i<=5: sum+=i; i=i+1
-print(sum)
+while i<=5: 
+    sum+=i
+    if i<5:
+        print(i, end=' + ')
+    else:
+        print(i,end=' ')
+    i=i+1
+
+print(f'= {sum}')
---
+++ loops/while loop/sum_to_n.py
@@ -0,0 +1,8 @@
+n = int(input('Enter a number : '))
+i, sum = 1, 0
+
+while i <= n:
+    sum += i # sum = sum + i
+    i += 1
+
+print(sum)
