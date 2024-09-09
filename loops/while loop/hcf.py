bcc2530419caa4b86d99888b1712709a2a518e1e
Athul K A <athulajithkumar16@gmail.com>
2024-09-09 09:56:17 +0530

    .

--- loops/while loop/hcf.py
+++ loops/while loop/hcf.py
@@ -1,8 +1,26 @@
 num1 = int(input('Enter a number : '))
 num2 = int(input('Enter another number : '))
-i = 1
+
+# METHOD 1
+#  i = 1
+# while i <= min(num1,num2):
+#     if num1 % i == 0 and num2 % i == 0:
+#         # print(i, end=' ')
+#         hcf= i
+#     i += 1
+
+# print(f'HCF of {num1} and {num2} is {hcf}')
+
+# METHOD 2
+
+i = min(num1, num2)
 
 while i <= min(num1,num2):
     if num1 % i == 0 and num2 % i == 0:
-        print(i, end=' ')
-    i += 1
+        hcf = i
+        break
+    i -=1
+print(hcf)
+
+# METHOD 3
+while 
