acfd1382dada860a464890fe9a7093ddebe1eefd
Athul K A <athulajithkumar16@gmail.com>
2024-09-09 08:43:52 +0530

    .

---
+++ loops/while loop/challenge4.py
@@ -0,0 +1,19 @@
+# 4. Ask for the name of somebody the user wants to invite to a party. After this, display the message "[name] has now been invited"
+# and add 1 to the count. Then ask if they want to invite somebody else. Keep repeating this until they no longer want to invite anyone
+# else to the party and then display how many people they have coming to the party.
+
+name = input('Enter name of person to invite : ')
+count = 0
+
+print(f'{name} has now been invited')
+count += 1
+
+choice = input('Do you want to invite someone ? ')
+
+while choice.lower() == 'y':
+    invite_name = input('Enter name of the person you want to invite : ')
+    print(f'{invite_name} has now been invited')
+    count += 1
+    choice = input('Do you want to add more? ')
+
+print(f'You invited {count} people')
