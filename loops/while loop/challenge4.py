a9b3f858e7f32adf28f674afde04aebbac6cf647
Athul K A <athulajithkumar16@gmail.com>
2024-09-09 09:10:12 +0530

    .

--- loops/while loop/challenge4.py
+++ loops/while loop/challenge4.py
@@ -2,18 +2,25 @@
 # and add 1 to the count. Then ask if they want to invite somebody else. Keep repeating this until they no longer want to invite anyone
 # else to the party and then display how many people they have coming to the party.
 
-name = input('Enter name of person to invite : ')
+invite_list = []
 count = 0
+name = input('Enter name of person to invite : ')
 
 print(f'{name} has now been invited')
+invite_list.append(name)
 count += 1
 
 choice = input('Do you want to invite someone ? (y/n) ')
 
+
 while choice.lower() == 'y':
     invite_name = input('Enter name of the person you want to invite : ')
     print(f'{invite_name} has now been invited')
+    invite_list.append(invite_name)
     count += 1
     choice = input('Do you want to add more? (y/n) ')
 
 print(f'You invited {count} people')
+# print(f'{invite_list} has been invited', sep=',')
+# print(f'{invite_list} has been invited', sep=',')
+print(*invite_list, sep=',')
---
+++ loops/while loop/factorial.py
@@ -0,0 +1,10 @@
+
+num = int(input('Enter the number : '))
+i = 1
+fact = 1
+
+while i <= num :
+    fact *= i
+    i += 1
+
+print(fact)
