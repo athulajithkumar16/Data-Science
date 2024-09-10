a04cfd697cf56840ac45b57aacfd94d44e646302
Athul K A <athulajithkumar16@gmail.com>
2024-09-10 08:44:37 +0530

    .

--- loops/for loop/intro.py
+++ loops/for loop/intro.py
@@ -4,6 +4,9 @@
 # print(range(0,100,2),end=' ')
 # print(range(50),end=' ')
 
+for i in range(10):
+    print(i)
+
 # PROGRAM TO PRINT EVEN NUMBERS using for loop and range()
 # for i in range(0,101,2):
 #     print(i, end=' ')
@@ -12,5 +15,6 @@
 # for i in range(1,101,2):
 #     print(i, end=' ')
 
-for i in range(100,0,-2):
-    print(i, end=' ')
+# for i in range(100,0,-2):
+#     print(i, end=' ')
+
