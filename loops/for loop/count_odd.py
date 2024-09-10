f38e076f4025ae7308efd3c96ad3e328aea48d3b
Athul K A <athulajithkumar16@gmail.com>
2024-09-10 10:03:20 +0530

    .

--- loops/for loop/count_odd.py
+++ loops/for loop/count_odd.py
@@ -4,7 +4,7 @@ count = 0
 # print(str(number)[::-1])
 for i in str(number):
 
-    count += 1 if int(i)%2 != 0 else c
-    # if int(i) % 2 !=0:
-    #     count+=1
+    # count += 1 if int(i)%2 != 0 else 
+    if int(i) % 2 !=0:
+        count+=1
 print(count)
