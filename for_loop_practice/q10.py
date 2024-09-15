d2258ff7b28da62a026a167af2b1f0dd78c3a6eb
Athul K A <athulajithkumar16@gmail.com>
2024-09-15 23:42:01 +0530

    .

--- for_loop_practice/q10.py
+++ for_loop_practice/q10.py
@@ -1,8 +1,7 @@
 # Example 10: Python program to check if a given number is an Armstrong number
 
-# n = 123 
-
 number = int(input('Enter a number : '))
+dummy_number = number
 sum = 0
 length = len(str(number))
 
@@ -11,8 +10,7 @@ for i in range(len(str(number))):
     sum += last_digit**length
     number //= 10
 
-
-if sum == number:
+if sum == dummy_number:
     print('is Armstrong Number')
 else:
     print('is Not Armstrong Number')
