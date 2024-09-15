0eb18186c6ce0a0a07a9095eff32bff63df1fa78
Athul K A <athulajithkumar16@gmail.com>
2024-09-15 23:50:40 +0530

    .

--- for_loop_practice/q11.py
+++ for_loop_practice/q11.py
@@ -1,6 +1,13 @@
 # Example 11: Python program to count the number of even and odd numbers from a series of numbers.
 
 num = list(map(int,input('Enter series of numbers : ').split()))
+#  [1,3,5,6,99,134,55]
+even, odd = 0, 0
 
-# inp = list(map(int,input().split()))
-print(num)
+for i in num:
+    if i % 2 == 0:
+        even+=1
+    else:
+        odd+=1
+
+print(f'{even} EVEN \n{odd} ODD')
