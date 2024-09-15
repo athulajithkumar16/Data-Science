128be1cd7bb5bcec51a6e59ae251e82c62981960
Athul K A <athulajithkumar16@gmail.com>
2024-09-15 23:48:09 +0530

    .

--- for_loop_practice/q11.py
+++ for_loop_practice/q11.py
@@ -1,3 +1,6 @@
 # Example 11: Python program to count the number of even and odd numbers from a series of numbers.
 
-num = int(input('Enter a series '))
+num = list(map(int,input('Enter series of numbers : ').split()))
+
+# inp = list(map(int,input().split()))
+print(num)
