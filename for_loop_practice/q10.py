7bce8cd70303919fea2ce73720cbd7812ab77501
Athul K A <athulajithkumar16@gmail.com>
2024-09-15 23:38:57 +0530

    .

--- for_loop_practice/q10.py
+++ for_loop_practice/q10.py
@@ -4,15 +4,13 @@
 
 number = int(input('Enter a number : '))
 sum = 0
-# print(len(str(number)))
+length = len(str(number))
 
-for i in range(1,len(str(number))):
+for i in range(len(str(number))):
     last_digit = number % 10
-    print(last_digit)
-    sum += last_digit**2
-    print(sum)
+    sum += last_digit**length
     number //= 10
-    print(number)
+
 
 if sum == number:
     print('is Armstrong Number')
