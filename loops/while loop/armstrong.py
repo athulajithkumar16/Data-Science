4b154f65c01122f03b307f7bc73d109c42f10ef5
Athul K A <athulajithkumar16@gmail.com>
2024-09-16 23:31:59 +0530

    .

--- loops/while loop/armstrong.py
+++ loops/while loop/armstrong.py
@@ -1,13 +1,16 @@
+number = int(input('Enter a number : '))
+dummy_number = number
+sum = 0
 
-
-
+while number < 0:
+    last_digit = number % 10
+    sum += last_digit**len(str(dummy_number))
+    number //=10
 
 
 
 
 # number = int(input('Enter a number : '))
-# dummy_number = number
-# sum = 0
 # length = len(str(number))
 
 # for i in range(len(str(number))):
