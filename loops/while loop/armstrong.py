8045c1fac0b29767ed53c7ea6115e8814771922f
Athul K A <athulajithkumar16@gmail.com>
2024-09-16 23:29:28 +0530

    .

---
+++ loops/while loop/armstrong.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+# number = int(input('Enter a number : '))
+# dummy_number = number
+# sum = 0
+# length = len(str(number))
+
+# for i in range(len(str(number))):
+#     last_digit = number % 10
+#     sum += last_digit**length
+#     number //= 10
+
+# if sum == dummy_number:
+#     print('is Armstrong Number')
+# else:
+#     print('is Not Armstrong Number')
