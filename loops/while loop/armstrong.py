8a04ac2317b8a311115e232330ff301eda1e1053
Athul K A <athulajithkumar16@gmail.com>
2024-09-16 23:33:37 +0530

    .

--- loops/while loop/armstrong.py
+++ loops/while loop/armstrong.py
@@ -2,23 +2,12 @@ number = int(input('Enter a number : '))
 dummy_number = number
 sum = 0
 
-while number < 0:
+while number > 0:
     last_digit = number % 10
     sum += last_digit**len(str(dummy_number))
     number //=10
 
-
-
-
-# number = int(input('Enter a number : '))
-# length = len(str(number))
-
-# for i in range(len(str(number))):
-#     last_digit = number % 10
-#     sum += last_digit**length
-#     number //= 10
-
-# if sum == dummy_number:
-#     print('is Armstrong Number')
-# else:
-#     print('is Not Armstrong Number')
+if sum == dummy_number:
+    print('is Armstrong Number')
+else:
+    print('is Not Armstrong Number')
