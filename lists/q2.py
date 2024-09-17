a6222132f10b6f690f866f245f7e00f17fec5995
Athul K A <athulajithkumar16@gmail.com>
2024-09-17 23:36:14 +0530

    .

--- lists/q2.py
+++ lists/q2.py
@@ -1,5 +1,9 @@
-list_ = [1,2,3,4,5,6,7,8,9]
+list_ = [0,1,2,3,4,5,6,7,8,9]
 print(list_)
 
 swap1 = int(input('Enter a position to swap :'))
 swap2 = int(input('Enter another position to swap :'))
+
+list_[swap1], list_[swap2] = list_[swap2], list_[swap1]
+
+print(list_)
