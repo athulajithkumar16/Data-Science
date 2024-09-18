d5e32079d9fdff0ee1f00bbcaab9f991bfa93ce7
Athul K A <athulajithkumar16@gmail.com>
2024-09-18 10:33:56 +0530

    row and clumn

--- loops/for loop/pattern.py
+++ loops/for loop/pattern.py
@@ -13,7 +13,19 @@
 # for i in range(5,-1,-1):
 #     print(i,end=' @ ')
 
-for i in range(1,5):
-    for j in range(1,5):
-        print(j, end=' ')
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(j, end=' ')
+#     print()
+
+# for row in range(5):
+#     for column in range(4):
+#         print('*', end='\t')
+#     print()
+
+for row in range(1,5):
+    for column in range(1,5):
+        print(column, end='\t')
+        if column == row:
+            break
     print()
