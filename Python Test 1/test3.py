39428ae0ae3591f1b46246920c37ce823b816386
Athul K A <athulajithkumar16@gmail.com>
2024-09-18 09:31:17 +0530

    .

--- Python Test 1/test3.py
+++ Python Test 1/test3.py
@@ -1,15 +1,15 @@
 string = input('Enter the string : ')
-reverse_string = ''
-dummy_string = ''
 
+# METHOD 1
+# reverse_string = ''
+# dummy_string = ''
+# position = string.index('o')
+# for i in range(position):
+#     reverse_string += string[i]
+# for i in range(position,len(string)):
+#     dummy_string += string[i]
+# print(reverse_string[::-1] + dummy_string)
+
+# METHOD 2
 position = string.index('o')
-
-for i in range(position):
-    reverse_string += string[i]
-
-for i in range(position,len(string)):
-    dummy_string += string[i]
-
-print(reverse_string[::-1] + dummy_string)
-
-
+print(string[:position:-1] + string[position:] )
---
+++ loops/for loop/pattern.py
@@ -0,0 +1,5 @@
+# print('good', end='')
+# print('morning')
+
+for i in range(5):
+    print(i, end=' ')
