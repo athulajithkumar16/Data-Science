4fe15f947ab053912508a00d7201e8b346edabb9
Athul K A <athulajithkumar16@gmail.com>
2024-09-18 09:49:27 +0530

    .

--- loops/for loop/pattern.py
+++ loops/for loop/pattern.py
@@ -10,5 +10,10 @@
 # for i in range(5,0,-1):
 #     print(i,end='@')
 
-for i in range(5,-1,-1):
-    print(i,end=' @ ')
+# for i in range(5,-1,-1):
+#     print(i,end=' @ ')
+
+for i in range(1,5):
+    for j in range(1,5):
+        print(j, end=' ')
+    print()
