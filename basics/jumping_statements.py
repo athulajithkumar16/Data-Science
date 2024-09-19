da3863e90f684e9ae2b71b62dac2ab05794e9ca1
Athul K A <athulajithkumar16@gmail.com>
2024-09-19 10:56:15 +0530

    .

---
+++ basics/jumping_statements.py
@@ -0,0 +1,13 @@
+# for i in range(1,10):
+#     print(i)
+#     if i==5: break
+
+# for i in range(1,10):
+#     if i==3:
+#         continue
+#     print(i)
+
+for i in range(1,20):
+    if i%2==0:
+        continue
+    print(i)
