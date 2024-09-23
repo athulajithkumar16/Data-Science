326b9af8e46c02b47c51da6c47e21200514bdca6
Athul K A <athulajithkumar16@gmail.com>
2024-09-23 09:39:48 +0530

    .

---
+++ functions/max.py
@@ -0,0 +1,11 @@
+def max(a,b):       # parameters
+    # if a > b:
+    #     print(f'{a} is greater')
+    # elif b > a:
+    #     print(f'{b} is greater')
+
+    # return f'{a} is greater' if a>b else f'{b} is greater'
+    print(f'{a} is greater' if a>b else f'{b} is greater')
+
+# print(max(2,3))
+max(10,4)       # arguments
