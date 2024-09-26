1341479ea4a74c422ab276e39d5c6cdeba5cd165
Athul K A <athulajithkumar16@gmail.com>
2024-09-26 10:18:15 +0530

    list

---
+++ collections/list/intro.py
@@ -0,0 +1,14 @@
+
+list_ = []
+print(type(list_))      # <class 'list'>
+
+list2 = ['suku', 12, 13, [1, 2, 3, 'alfin']]
+
+list2.append(20)        
+
+print(list2)        # ['suku', 12, 13, [1, 2, 3, 'alfin'], 20]
+
+name = 'Athena'
+list_name = list(name)
+
+print(list_name)
